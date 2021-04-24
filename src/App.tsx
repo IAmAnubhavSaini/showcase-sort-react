d79c2e4c235b58ccb746f4e785999aac40d9ec35
Anubhav Saini <iamanubhavsaini@gmail.com>
2021-04-24 23:13:48 +0530

    works

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Container, Row, Col, Nav, FormControl, Form, Navbar, Button, Jumbotron} from 'react-bootstrap';
 import BubbleSort from "./BubbleSort";
-import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import SelectionSort from "./SelectionSort";
 
 function App() {
+    const [component, setComponent] = useState(<Greet/>);
+
     return (
-        <Router basename="/showcase-sort-react">
-            <Container fluid className="App">
-                <Row>
-                    <Col>
-                        <Navbar bg="danger" variant="dark">
-                            <Navbar.Brand href="/">Sorting visualisations</Navbar.Brand>
-                            <Nav className="mr-auto">
-                                <Nav.Link href="/bubble">Bubble</Nav.Link>
-                                <Nav.Link href="/selection">Selection</Nav.Link>
-                                {/*<Nav.Link href="/insertion">Insertion</Nav.Link>*/}
-                            </Nav>
-                        </Navbar>
-                    </Col>
-                </Row>
 
-                <Row><Col>
+        <Container fluid className="App">
+            <Row>
+                <Col>
+                    <Navbar bg="danger" variant="dark">
+                        <Navbar.Brand href="/">Sorting visualisations</Navbar.Brand>
+                        <Nav className="mr-auto">
+                            <Nav.Link onClick={() => setComponent(<BubbleSort/>)}>Bubble</Nav.Link>
+                            <Nav.Link onClick={() => setComponent(<SelectionSort/>)}>Selection</Nav.Link>
+                            {/*<Nav.Link href="/insertion">Insertion</Nav.Link>*/}
+                        </Nav>
+                    </Navbar>
+                </Col>
+            </Row>
 
-                    <Switch>
-                        <Route exact path="/">
-                            <Jumbotron>
-                                <h1>Hello to sorting visualisations</h1>
-                                <p>Click on the links to go see the visualisation.</p>
-                            </Jumbotron>
-                        </Route>
-                        <Route path="/bubble" component={BubbleSort}/>
-                        <Route path="/selection" component={SelectionSort}/>
-                        <Redirect to="/"/>
-                    </Switch>
-
-                </Col></Row>
-            </Container>
-        </Router>
+            <Row><Col>
+                {component}
+            </Col></Row>
+        </Container>
     );
 }
 
 export default App;
+
+function Greet() {
+    return (<Jumbotron>
+        <h1>Hello to sorting visualisations</h1>
+        <p>Click on the links to go see the visualisation.</p>
+    </Jumbotron>);
+}
